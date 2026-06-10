0a9eec7a8684235ad56c56dfd861460f0a1e6eb0
fuzzefer <fuzzefer.real0@gmail.com>
2026-06-10 22:26:34 +0700

    added fb pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,11 @@ import "./globals.css";
 import { Navbar } from "@/components/zpath/Navbar";
 import { Footer } from "@/components/zpath/Footer";
 import { Providers } from "@/components/zpath/Providers";
+import { MetaPixelPageViewTracker } from "@/components/zpath/MetaPixel";
 import { Analytics } from "@vercel/analytics/next";
 import { getMetadataBase, SITE_NAME } from "@/lib/seo";
+import { Suspense } from "react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: getMetadataBase(),
@@ -28,9 +31,48 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+
   return (
     <html lang="vi">
+      <head>
+        {metaPixelId ? (
+          <Script
+            id="meta-pixel"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${JSON.stringify(metaPixelId)});
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+        ) : null}
+      </head>
       <body className="min-h-screen flex flex-col bg-background font-sans text-foreground">
+        {metaPixelId ? (
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${encodeURIComponent(metaPixelId)}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        ) : null}
+        <Suspense fallback={null}>
+          <MetaPixelPageViewTracker pixelId={metaPixelId} />
+        </Suspense>
         <Providers>
           <Navbar />
           
